12296f6dfa933e2bc710895c003779a8553ed819
AlexanderHo <chupakachi1@gmail.com>
2021-03-24 15:51:02 +0700

    check

--- src/com/khai/mathutil/MathUtity.java
+++ src/com/khai/mathutil/MathUtity.java
@@ -25,7 +25,7 @@ if(n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid argument , n must be between 0..20");
         if(n==0 || n==1)
             return 1;
-        long result = 0;
+        long result = 1;
         for ( int i=2 ; i<=n;i++)
             result = result*i;
         return result;
